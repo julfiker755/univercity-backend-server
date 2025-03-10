221c234832f439b84ed2115296811737c513e115
julfiker755 <julfiker755.bd@gmail.com>
2025-03-10 23:21:56 +0600

    zod error validation added

--- src/app/errors/globalErrorhandler.ts
+++ src/app/errors/globalErrorhandler.ts
@@ -1,11 +1,46 @@
-import {NextFunction, Request, Response } from 'express';
+import {ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import httpStatus from "http-status";
+import { ZodError } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
-const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunction):any=>{
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+const globalErrorHandler:ErrorRequestHandler=(err:any,req:Request,res:Response,next:NextFunction):any=>{
+    let statusCode= 500 
+    let message= err.message ||  "Something went Wrong!"
+     
+   
+    let errorSources:TErrorSource=[{
+        path:'',
+        message:''
+    }]
+
+    const handleZodError=(err:ZodError)=>{
+        const errorSources=err.issues.map((issue:any)=>{
+            return {
+                path:issue?.path[issue.path.length-1],
+                message:issue.message
+            }
+        })
+
+        return {
+            statusCode:httpStatus.BAD_REQUEST,
+            message:"Validation Error",
+            errorSources,
+        }
+    }
+
+    if (err instanceof ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
+    }
+
+    return res.status(statusCode).json({
         success:false,
-        message:err.message ||  "Something went Wrong!",
-        error:err,
+        message:message,
+        errorSources:errorSources,
+        stack:config.node_env === "development" ? err?.stack :null
        })
 }
 
--- src/app/modules/academic-deparment/deparment.validation.ts
+++ src/app/modules/academic-deparment/deparment.validation.ts
@@ -1,14 +1,27 @@
 import { z } from 'zod';
 
 const deparmentSchema = z.object({
-    name:z.string().min(1,{ message: "Name is required" }),
-    academicFaculty:z.string().min(1,{ message: "academicFaculty is required" }),
-
+    name: z
+    .string({
+      invalid_type_error: 'Academic department must be string',
+      required_error: 'Name is required',
+    }),
+    academicFaculty:z.string({
+        invalid_type_error: 'academicFaculty is required',
+        required_error: 'AcademicFaculty is required',
+      })
 });
-const deparmentupdateSchema = z.object({
-    name:z.string().min(1,{ message: "Name is required" }).optional(),
-    academicFaculty:z.string().min(1,{ message: "academicFaculty is required" }).optional(),
 
+const deparmentupdateSchema = z.object({
+    name: z
+    .string({
+      invalid_type_error: 'Academic department must be string',
+      required_error: 'Name is required',
+    }).optional(),
+    academicFaculty:z.string({
+        invalid_type_error: 'academicFaculty is required',
+        required_error: 'AcademicFaculty is required',
+      }).optional()
 });
 
 export const deparmentValidation = {
